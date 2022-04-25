6492f21019ed0e03c31081e35409593015a0a4a4
Alireza Valizadeh <alirezavalizadeh1999@gmail.com>
2022-04-25 12:26:02 +0430

    Update ddc.js

--- functions/integrations/ddc/ddc.js
+++ functions/integrations/ddc/ddc.js
@@ -2,7 +2,7 @@
   const API = await APILoader.create("glo3d");
   // Loads a JavaScript file
   API.loadJS(
-    "https://cdn.jsdelivr.net/gh/alireza-valizadeh/glo3d-script@24b987df37f75aff9b32bca754a2be73a5232aa9/functions/integrations/base.js"
+    "https://cdn.jsdelivr.net/gh/alireza-valizadeh/glo3d-script@24b987df37f75aff9b32bca754a2be73a5232aa9/functions/integrations/ddc/ddc.js"
   ).then(() => async (APILoader) => {
     console.log("loadeddddddd");
   });
